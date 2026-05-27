6374f3f6a0b90b607f2e510bc6c20ae1fac77041
Shemaiah M <shemkmmillard@gmail.com>
2026-05-27 12:17:53 +0100

    Add files via upload

---
+++ hello.world.py
@@ -0,0 +1,3 @@
+print("Yellow world")
+print("New start to python for software development and for cyber security, using Mike McGrath book")
+print("hopefully this time ill stick to it and continue for path forward!")
