ca6a7cbac0b70bd2a5cf6843445bf3254075d0af
Shemaiah M <shemkmmillard@gmail.com>
2026-05-27 12:39:23 +0100

    Add files via upload

---
+++ variablesBasic.py
@@ -0,0 +1,24 @@
+failed_subjects = "6"
+name = "Eric"
+#when using variables we use camel case or underscore, we also use descriptive words
+print("Dear Mrs Badger!")
+print("We have to contact you about your son " + name + ".")
+name = "John"
+print("Your son " + name + " is failing " + failed_subjects + " subjects.")
+print( name + " will need to redo all " + failed_subjects +" courses.")
+name= "Eric"
+print(name + " is doing well in Geography.")
+
+
+#here im using the simple old school var, in js "var" doesn't work
+# or var is more so confusing and obsolete .
+var = 8
+print(var)
+print(var + 8 )
+var = 8 - 4
+print(var)
+var = 16 * 3
+print(var)
+
+newVar = "Python in easy steps!"
+print(newVar)
