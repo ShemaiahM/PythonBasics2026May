baae02f91c337d90c5858b8a4dff320d7ca10c76
Shemaiah M <shemkmmillard@gmail.com>
2026-05-29 15:21:00 +0100

    Scrimba Python Variables

---
+++ Variables.py
@@ -0,0 +1,8 @@
+
+failed_subjects = "6"
+name ="john"
+print('Dear Mrs Badger')
+print('your son ' +name+ ' is failing '+ failed_subjects+' subjects')
+print(name + ' will need to redo ' + failed_subjects+ ' courses')
+name="Eric"
+print(name +  'is doing well in geography')
